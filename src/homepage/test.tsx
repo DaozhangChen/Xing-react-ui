f0c8d6cb7f72c9923b5a47152dea818bb2ffffcc
chendaozhang <1205308740@qq.com>
2022-12-18 23:52:42 +0800

    完成Svg节点的ts类型书写

--- src/homepage/test.tsx
+++ src/homepage/test.tsx
@@ -1,18 +1,21 @@
-import React, {useEffect, useState} from "react";
+import React, { ReactNode } from 'react'
 import s from './test.module.scss'
 
-type Prop={
-    name:string
+type Prop = {
+    name: string
 }
-
-const Test = (props:Prop)=>{
-    const modules = import.meta.glob('../assets/icons/*.svg', { eager: true })
-    const path = `../assets/icons/${props.name}.svg`
+type SvgImport = {
+    [path: string]: { default: React.ComponentType }
+}
+const Test = (props: Prop) => {
+    const modules: SvgImport = import.meta.glob('../assets/icons/*.svg', { eager: true })
+    console.log(modules)
+    const path: string = `../assets/icons/${props.name}.svg`
     const All = modules[path].default
     return (
         <div className={s.icon}>
-        <All />
-        <div>test</div>
+            <All />
+            <div>test</div>
         </div>
     )
 }
