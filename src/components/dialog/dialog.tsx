9d89e9db285806708ccc7b27dc0b74cc0def8796
chendaozhang <1205308740@qq.com>
2022-12-20 16:16:31 +0800

    给dialog添加closeIcon

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -4,6 +4,7 @@ import React, {
     ReactNode
 } from "react";
 import {createPortal,render} from "react-dom";
+import Icon from "../icon/icon";
 type Prop={
     visible:boolean
     maskOnClick?:MouseEventHandler<HTMLDivElement>
@@ -15,12 +16,14 @@ type Prop={
     okText?:string
     customFooter?:ReactNode[]
 }
+
 const Dialog=(props:Prop)=>{
     return createPortal((
         <>{props.visible?
             <div className={s.xing_ui_dialog_wrapper}>
                 <div className={s.xing_ui_dialog_mask} onClick={props.maskOnClick}/>
                 <div className={s.xing_ui_dialog_main}>
+                    <Icon name='close' className={s.xing_ui_dialog_icon}/>
                     <header className={s.xing_ui_dialog_title}>{props.titleText?props.titleText:'提醒'}</header>
                     {props.children}
                     <footer className={props.customFooter? undefined :s.xing_ui_dialog_button}>{
