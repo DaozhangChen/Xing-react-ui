1188ed2055f724cf3685a9f1b8adcbf6e985676a
chendaozhang <1205308740@qq.com>
2022-12-21 17:19:09 +0800

    修改了icon和增加了主页背景

--- src/homepage/homePage.tsx
+++ src/homepage/homePage.tsx
@@ -3,17 +3,28 @@ import Header from "../components/layout/header";
 import Content from "../components/layout/content";
 import Footer from "../components/layout/footer";
 import Icon from "../components/icon/icon";
-
+import s from './homePage.module.scss'
+import background from '../assets/background.mp4'
+import mainLogo from '../assets/mainLogo.png'
 const homePage =()=>{
     return (
         <>
         <Layout>
-            <Header>
-                <Icon name="star" />
-
-
+            <Header className={s.layoutHeader}>
+                <div className={s.iconAndTitle}>
+                    <img src={mainLogo} className={s.mainLogo}/>
+                    <h2>XING UI</h2>
+                </div>
+                <div className={s.headerRight}>
+                    <ul>
+                        <li>Vue版</li>
+                    </ul>
+                    <Icon className={s.icon} name="github" />
+                </div>
             </Header>
-            <Content></Content>
+            <Content>
+                <video src={background} autoPlay={true} className={s.background} loop={true}/>
+            </Content>
             <Footer></Footer>
         </Layout>
         </>
