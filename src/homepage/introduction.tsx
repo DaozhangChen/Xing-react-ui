43a36a8afe4ea3baff20b964fe982c43661c653a
chendaozhang <1205308740@qq.com>
2022-12-22 00:50:38 +0800

    更快捷的生成li

--- src/homepage/introduction.tsx
+++ src/homepage/introduction.tsx
@@ -3,20 +3,32 @@ import Layout from "../components/layout/layout";
 import Footer from "../components/layout/footer";
 import Aside from "../components/layout/aside";
 import Content from "../components/layout/content";
-import React from "react";
+import React, {MouseEventHandler, useState} from "react";
 import ShareHeader from "./share/shareHeader";
-import {Link, Outlet, Route, Routes} from "react-router-dom";
+import {Link, Outlet, useMatch, useParams} from "react-router-dom";
+import {listObject} from "./share/listObject";
 const introduction =()=>{
+    const [selected,setSelected]=useState('')
+    const parmas =useParams()
+    const onSelect:MouseEventHandler=(e:React.MouseEvent)=>{
+        console.log(e.currentTarget.id)
+        console.log(parmas)
+
+    }
     return (
         <>
             <Layout>
                 <ShareHeader />
                 <Layout className={s.mainLayout}>
                     <Aside className={s.mainAside}>
+                        <div>所有组件</div>
                         <ul>
-                            <li><Link to="/introduction/icon"><span>Icon</span> <span>图标</span></Link></li>
-                            <li><Link to="/introduction/dialog"><span>Dialog</span> <span>对话框</span></Link></li>
-                            <li><Link to="/introduction/layout"><span>Layout</span> <span>布局</span></Link></li>
+                            {listObject.map(li=><Link key={li.eName} to={`/introduction/${li.eName[0].toLowerCase() + li.eName.substring(1)}`}>
+                                <li  id={li.eName} onClick={onSelect}>
+                                    <span>{li.eName}</span>
+                                    <span className={s.text}>{li.text}</span>
+                                </li>
+                            </Link>)}
                         </ul>
                     </Aside>
                     <Content className={s.mainContent}>
---
+++ src/homepage/share/listObject.tsx
@@ -0,0 +1,5 @@
+export const listObject=[
+    {eName:'Icon',text:'图标'},
+    {eName:'Dialog',text:'对话框'},
+    {eName:'Layout',text:'布局'}
+]
