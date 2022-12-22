f7cde31733322ab85de6e7c28c70b851e355a7c0
chendaozhang <1205308740@qq.com>
2022-12-22 20:06:38 +0800

    修改了Icon的内部代码，将icon分为了单色与多色，并不会冲突

--- src/components/icon/icon.example.tsx
+++ src/components/icon/icon.example.tsx
@@ -4,18 +4,28 @@ import tsx from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript'
 import Icon from "./icon";
 import s from './icon.example.module.scss'
 const iconExample = ()=>{
-    let list = []
-    const modules = import.meta.glob('@assets/icons/*.svg')
-    for (const path in modules){
+    let oneColorList = []
+    let moreColorList = []
+    const oneColorFiles = import.meta.glob('@assets/icons/oneColorIcon/*.svg')
+    for (const path in oneColorFiles){
         const regex:RegExp =/.*\/(.+)\./
         const match = regex.exec(path)
-        list.push(match?.[1])
+        oneColorList.push(match?.[1])
+    }
+    const moreColorFiles = import.meta.glob('@assets/icons/moreColorIcon/*.svg')
+    for (const path in moreColorFiles){
+        const regex:RegExp =/.*\/(.+)\./
+        const match = regex.exec(path)
+        moreColorList.push(match?.[1])
     }
     HighLight.registerLanguage('tsx',tsx)
     return (
         <>
         <h1>Icon 组件</h1>
-            {list.map(iconName=><Icon key={iconName} name={iconName||''} className={s.icon}/>)}
+            <h2>单色</h2>
+            {oneColorList.map(iconName=><Icon key={iconName} name={iconName||''} className={s.icon}/>)}
+            <h2>多色</h2>
+            {moreColorList.map(iconName=><Icon key={iconName} name={iconName||''} className={s.icon}/>)}
             <HighLight style={style}>
                 {`<div>321<div>
 console.log('1')
--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -14,9 +14,11 @@ type SvgImport = {
     [path: string]: { default: React.ComponentType<IconProp> }
 }
 const Icon =(props:Prop)=>{
-  const modules: SvgImport = import.meta.glob('@assets/icons/*.svg', { eager: true })
-  const path: string = `/src/assets/icons/${props.name}.svg`
-  const IconComponent = modules[path].default
+  const moreColorList: SvgImport = import.meta.glob('@assets/icons/moreColorIcon/*.svg', { eager: true })
+  const oneColorIconList:SvgImport = import.meta.glob('@assets/icons/oneColorIcon/*.svg',{eager:true})
+  const one:string = `/src/assets/icons/oneColorIcon/${props.name}.svg`
+  const more: string = `/src/assets/icons/moreColorIcon/${props.name}.svg`
+  const IconComponent = moreColorList[more]?.default || oneColorIconList[one]?.default
   const CustomSVG = props.component
     return (<>
             {/*// @ts-ignore*/}
