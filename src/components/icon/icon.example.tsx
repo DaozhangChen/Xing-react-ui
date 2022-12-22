292dd75f96c4f27ff08a620e931e6274579b075c
chendaozhang <1205308740@qq.com>
2022-12-22 21:28:47 +0800

    完成Icon组件界面介绍

--- src/components/icon/icon.example.tsx
+++ src/components/icon/icon.example.tsx
@@ -22,15 +22,95 @@ const iconExample = ()=>{
     return (
         <>
         <h1>Icon 组件</h1>
-            <h2>单色</h2>
-            {oneColorList.map(iconName=><Icon key={iconName} name={iconName||''} className={s.icon}/>)}
-            <h2>多色</h2>
-            {moreColorList.map(iconName=><Icon key={iconName} name={iconName||''} className={s.icon}/>)}
+            <p>Icon组件可以让你及其轻松的使用各种图标，你可以通过传入不同的图标名称来渲染对应的图标，也可以自定义图标的样式，例如颜色、大小等。</p>
+            <h2>图标列表</h2>
+            <h3>单色图标</h3>
+            <div className={s.iconCardWrapper}>
+            {oneColorList.map(iconName=>
+                <div key={iconName} className={s.iconCard}>
+                <Icon  name={iconName||''} className={s.icon}/>
+                <div>{iconName}</div>
+                </div>
+            )}
+            </div>
+            <h3>多色图标</h3>
+            <div className={s.iconCardWrapper}>
+            {moreColorList.map(iconName=>
+                <div key={iconName} className={s.iconCard}>
+                <Icon key={iconName} name={iconName||''} className={s.icon}/>
+                <div>{iconName}</div>
+                </div>
+            )}
+            </div>
+            <h2>代码演示</h2>
+            <h3>基本用法</h3>
+            <p>使用Icon组件的name属性,传入字符串，即可使用，以下的两个标签，就是通过Icon引入的</p>
+            <div>
+                <Icon name='add' className={s.icon}/>
+                <Icon name='商店' className={s.icon}/>
+            </div>
+            <div className={s.changeSize}>
             <HighLight style={style}>
-                {`<div>321<div>
-console.log('1')
-`}
+                {`
+const myComponent=()=>{
+    return (
+        <>
+        <Icon name='add' />
+        <Icon name='商店' />
+        </>
+    )
+}
+export default myComponent
+                `}
             </HighLight>
+            </div>
+            <h3>修改Icon</h3>
+            <p>将自己的CSS属性，通过Icon的className进行属性覆盖，即可修改原有的属性，下面我将使用fill属性来修改Icon原有的颜色</p>
+            <div>
+                <Icon name='add' style={{fill:"red",height:36,width:36}}/>
+                <Icon name='close' style={{fill:"blue",height:36,width:36}} />
+            </div>
+            <div className={s.changeSize}>
+                <HighLight style={style}>
+                    {`
+import './style.css'
+
+//   style.css
+//
+// .iconRed{
+//     fill: red;
+// }
+// .iconBlue{
+//     fill: blue;
+// }
+const myComponent=()=>{
+    return (
+        <>
+        <Icon name='add' className='iconRed'/>
+        <Icon name='close' className='iconBlue' />
+        </>
+    )
+}
+                    `}
+                </HighLight>
+            </div>
+            <h3>自定义Icon</h3>
+            <p>可以通过Icon的component标签，自定义个性化Icon</p>
+            <div className={s.changeSize}>
+                <HighLight style={style}>
+                    {`
+import mySvg from "svg的路径"
+
+const myComponent=()=>{
+    return (
+        <>
+        <Icon component={mySvg}/>
+        </>
+    )
+}
+                    `}
+                </HighLight>
+            </div>
         </>
     )
 }
---
+++ src/helper/lll.tsx
@@ -0,0 +1,11 @@
+import Icon from "../components/icon/icon";
+import mySvg from "svg的路径"
+
+const myComponent=()=>{
+    return (
+        <>
+        <Icon component={mySvg}/>
+        </>
+    )
+}
+export default myComponent
