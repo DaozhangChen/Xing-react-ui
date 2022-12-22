d5832c74e681ded8d987d8efeb2bc1e8995d5f49
chendaozhang <1205308740@qq.com>
2022-12-22 16:20:21 +0800

    成功匹配路由，并且能够高亮匹配的组件

---
+++ src/helper/toLowerName.tsx
@@ -0,0 +1,4 @@
+const toLowerName=(name:string)=>{
+    return name[0].toLowerCase()+name.substring(1)
+}
+export default  toLowerName
--- src/homepage/introduction.tsx
+++ src/homepage/introduction.tsx
@@ -1,18 +1,23 @@
 import s from './introduction.module.scss'
 import Layout from "../components/layout/layout";
-import Footer from "../components/layout/footer";
 import Aside from "../components/layout/aside";
 import Content from "../components/layout/content";
-import React, {MouseEventHandler, useState} from "react";
+import React, {MouseEventHandler, useEffect,  useState} from "react";
 import ShareHeader from "./share/shareHeader";
-import {Link, Outlet, useMatch, useParams} from "react-router-dom";
+import {Link, Outlet, useMatch} from "react-router-dom";
 import {listObject} from "./share/listObject";
+import toLowerName from "../helper/toLowerName";
 const introduction =()=>{
     const [selected,setSelected]=useState('')
-    const parmas =useParams()
+    const match =useMatch('/introduction/:componentName')
+    useEffect(()=>{
+        //让刷新页面后也能使Aside被选中部分高亮
+        if (match?.params && match.params.componentName){
+            setSelected(match.params.componentName)
+        }
+    },[match])
     const onSelect:MouseEventHandler=(e:React.MouseEvent)=>{
-        console.log(e.currentTarget.id)
-        console.log(parmas)
+        setSelected(e.currentTarget.id)
     }
     return (
         <>
@@ -22,9 +27,9 @@ const introduction =()=>{
                     <Aside className={s.mainAside}>
                         <div>所有组件</div>
                         <ul>
-                            {listObject.map(li=><Link key={li.eName} to={`/introduction/${li.eName[0].toLowerCase() + li.eName.substring(1)}`}>
-                                <li id={li.eName} onClick={onSelect}>
-                                    <span>{li.eName}</span>
+                            {listObject.map(li=><Link key={li.eName} to={`/introduction/${toLowerName(li.eName)}`}>
+                                <li id={li.eName} onClick={onSelect} className={toLowerName(li.eName)===selected?s.selected:undefined}>
+                                    <span>{li.eName + ' '}</span>
                                     <span className={s.text}>{li.text}</span>
                                 </li>
                             </Link>)}
