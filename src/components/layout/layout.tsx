e8585a540d9da5b9fa077bd922b545a724569217
chendaozhang <1205308740@qq.com>
2022-12-23 16:07:42 +0800

    修改了layout内容以及增加了点击回到首页

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -9,8 +9,10 @@ const ezName = easyUseClassName('xing_ui_layout')
 const Layout=(props:Prop)=>{
     const {className,...rest} = props
     const children = props.children as Array<ReactElement>
-    const hasAside = children.length &&
-        children.reduce((result,node) => result || node.type === Aside,false)
+    const hasAside = 'length' in children &&
+        children.reduce((result, node) => result || node.type === Aside, false);
+
+
     return <div className={ezName({'': true ,hasAside},{extra:className})} {...rest}>{props.children}</div>
 }
 
--- src/homepage/share/shareHeader.tsx
+++ src/homepage/share/shareHeader.tsx
@@ -3,14 +3,17 @@ import mainLogo from "@assets/mainLogo.png";
 import Icon from "../../components/icon/icon";
 import Header from "../../components/layout/header";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const shareHeader=()=>{
     return <>
     <Header className={s.layoutHeader}>
+        <Link to='/'>
         <div className={s.iconAndTitle}>
             <img src={mainLogo} className={s.mainLogo}/>
             <h2>XING UI</h2>
         </div>
+        </Link>
         <div className={s.headerRight}>
             <ul>
                 <li>Vue版</li>
