0dd2f57aa6f21247f084cba37f615351a1eb176f
chendaozhang <1205308740@qq.com>
2022-12-26 17:35:39 +0800

    弄好了声明文件。。

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, sortUserPlugins } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteSvgr from "vite-plugin-svgr";
 import * as path from "path";
+import typescript from '@rollup/plugin-typescript'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    lib:{
-      entry:path.resolve(__dirname,'./src/lib/index.tsx'),
-      name:'xing-react-ui',
-      fileName:'xing-react-ui',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/lib/index.tsx'),
+      name: 'xing-react-ui',
+      fileName: 'xing-react-ui',
     },
-    rollupOptions:{
-      external:['react'],
-      output:{
-        dir:'dist/lib',
-        globals:{
-          react:'React'
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        dir: 'dist/lib',
+        globals: {
+          react: 'react',
+          "react-dom": "react-dom"
         }
-      }
+      },
+      plugins: [
+        typescript({
+          tslib: resolve('typescript'),
+          // include: resolve(__dirname, 'src/lib/**'),
+          exclude: [
+            resolve(__dirname, 'src/homepage/**'),
+            resolve(__dirname, 'src/components/**'),
+            resolve(__dirname, 'src/helper/**')
+          ],
+          outDir: resolve(__dirname, 'dist/lib')
+        })
+      ]
     }
   },
-  resolve:{
-    alias:{
-      '@assets':path.resolve(__dirname,"./src/assets")
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, "./src/assets")
     }
   },
   plugins: [
-      react(),
+    react(),
     viteSvgr({
-      exportAsDefault:true
-    })
+      exportAsDefault: true
+    }),
   ],
 })
