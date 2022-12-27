ed3caabe93f806f50d6a0b9c73682a3189b96025
chendaozhang <1205308740@qq.com>
2022-12-27 14:52:49 +0800

    修改shareHeader使其兼容

--- src/homepage/share/shareHeader.tsx
+++ src/homepage/share/shareHeader.tsx
@@ -3,25 +3,28 @@ import mainLogo from "@assets/mainLogo.png";
 import Icon from "../../lib/icon/icon";
 import Header from "../../lib/layout/header";
 import React, {Dispatch, SetStateAction} from "react";
-import {Link} from "react-router-dom";
+import {Link, useMatch} from "react-router-dom";
 
 
 
 interface Prop{
-    controlMenu:Dispatch<SetStateAction<boolean>>
-    visible:boolean
+    controlMenu?:Dispatch<SetStateAction<boolean>>
+    visible?:boolean
 }
 const shareHeader=(props:Prop)=>{
     const onClose = ()=>{
-        props.controlMenu(!props.visible)
+        props.controlMenu?.(!props.visible)
     }
+    const match= useMatch('/')
+    const windowWidth = window.innerWidth
+    console.log(match)
     return <>
     <Header className={s.layoutHeader}>
-        <Icon name="more" className={s.menuIcon} onClick={onClose}/>
+        {match ? null : <Icon name="more" className={s.menuIcon} onClick={onClose}/>}
         <Link to='/'>
         <div className={s.iconAndTitle}>
             <img src={mainLogo} className={s.mainLogo}/>
-            <h2>XING UI</h2>
+            {match && windowWidth< 700 ? <h2>XING UI</h2>:null}
         </div>
         </Link>
         <div className={s.headerRight}>
