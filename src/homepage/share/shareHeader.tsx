c9e1adff76b5cec4bbd2b325438f82152200cbad
chendaozhang <1205308740@qq.com>
2022-12-27 15:12:39 +0800

    完美做到改变屏幕宽度时shareHeader发生变化

--- src/homepage/share/shareHeader.tsx
+++ src/homepage/share/shareHeader.tsx
@@ -2,8 +2,9 @@ import s from "./shareHeader.module.scss";
 import mainLogo from "@assets/mainLogo.png";
 import Icon from "../../lib/icon/icon";
 import Header from "../../lib/layout/header";
-import React, {Dispatch, SetStateAction} from "react";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {Link, useMatch} from "react-router-dom";
+import {ifError} from "assert";
 
 
 
@@ -12,19 +13,30 @@ interface Prop{
     visible?:boolean
 }
 const shareHeader=(props:Prop)=>{
+    const [titleVisible,setTitleVisible] = useState(false)
     const onClose = ()=>{
         props.controlMenu?.(!props.visible)
     }
-    const match= useMatch('/')
+    const matchHomepage= useMatch('/')
+    const matchIntroPage = useMatch('/introduction/:id')
     const windowWidth = window.innerWidth
-    console.log(match)
+    useEffect(()=>{
+        if (matchHomepage && windowWidth<700){
+            setTitleVisible(true)
+        }else if(windowWidth >= 700){
+            setTitleVisible(true)
+        }
+        if (matchIntroPage && windowWidth<700){
+            setTitleVisible(false)
+        }
+    },[windowWidth])
     return <>
     <Header className={s.layoutHeader}>
-        {match ? null : <Icon name="more" className={s.menuIcon} onClick={onClose}/>}
+        {matchHomepage ? null : <Icon name="more" className={s.menuIcon} onClick={onClose}/>}
         <Link to='/'>
         <div className={s.iconAndTitle}>
             <img src={mainLogo} className={s.mainLogo}/>
-            {match && windowWidth< 700 ? <h2>XING UI</h2>:null}
+            {titleVisible ? <h2>XING UI</h2>:null}
         </div>
         </Link>
         <div className={s.headerRight}>
