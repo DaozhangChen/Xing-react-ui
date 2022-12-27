00260278e870bbc608dd11640f5973085c2f8028
chendaozhang <1205308740@qq.com>
2022-12-27 21:33:19 +0800

    取消子路由懒加载

--- src/router.tsx
+++ src/router.tsx
@@ -2,18 +2,18 @@ import {createHashRouter,  redirect,  RouterProviderProps} from "react-router-do
 import React, {lazy} from "react";
 // import HomePage from "./homepage/homePage";
 // import Introduction from "./homepage/introduction";
-// import IconExample from "./components/examples/icon.example";
-// import DialogExample from "./components/examples/dialog.example";
-// import LayoutExample from "./components/examples/layout.example";
+import IconExample from "./components/examples/icon.example";
+import DialogExample from "./components/examples/dialog.example";
+import LayoutExample from "./components/examples/layout.example";
 import Example from "./homepage/example";
-// import Usage from "./components/commons/usage";
+import Usage from "./components/commons/usage";
 
 const HomePage = React.lazy(()=>import("./homepage/homePage"))
 const Introduction = React.lazy(()=>import("./homepage/introduction"))
-const IconExample = React.lazy(()=>import("./components/examples/icon.example"))
-const DialogExample = React.lazy(()=>import("./components/examples/dialog.example"))
-const LayoutExample = React.lazy(()=>import("./components/examples/layout.example"))
-const Usage = React.lazy(()=>import("./components/commons/usage"))
+// const IconExample = React.lazy(()=>import("./components/examples/icon.example"))
+// const DialogExample = React.lazy(()=>import("./components/examples/dialog.example"))
+// const LayoutExample = React.lazy(()=>import("./components/examples/layout.example"))
+// const Usage = React.lazy(()=>import("./components/commons/usage"))
 
 // @ts-ignore
 const router:RouterProviderProps = createHashRouter([
