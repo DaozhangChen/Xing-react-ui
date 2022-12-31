f7260fb50f073cb5337330d4f5cc1e4320cbe771
chendaozhang <1205308740@qq.com>
2022-12-31 21:56:43 +0800

    修改vue版地址

--- src/homepage/share/shareHeader.tsx
+++ src/homepage/share/shareHeader.tsx
@@ -2,49 +2,49 @@ import s from "./shareHeader.module.scss";
 import mainLogo from "@assets/mainLogo.png";
 import Icon from "../../lib/icon/icon";
 import Header from "../../lib/layout/header";
-import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
-import {Link, useMatch} from "react-router-dom";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Link, useMatch } from "react-router-dom";
 
 
 
-interface Prop{
-    controlMenu?:Dispatch<SetStateAction<boolean>>
-    visible?:boolean
+interface Prop {
+    controlMenu?: Dispatch<SetStateAction<boolean>>
+    visible?: boolean
 }
-const shareHeader=(props:Prop)=>{
-    const [titleVisible,setTitleVisible] = useState(false)
-    const onClose = ()=>{
+const shareHeader = (props: Prop) => {
+    const [titleVisible, setTitleVisible] = useState(false)
+    const onClose = () => {
         props.controlMenu?.(!props.visible)
     }
-    const matchHomepage= useMatch('/')
+    const matchHomepage = useMatch('/')
     const matchIntroPage = useMatch('/introduction/:id')
     const windowWidth = window.innerWidth
-    useEffect(()=>{
-        if (matchHomepage && windowWidth<700){
+    useEffect(() => {
+        if (matchHomepage && windowWidth < 700) {
             setTitleVisible(true)
-        }else if(windowWidth >= 700){
+        } else if (windowWidth >= 700) {
             setTitleVisible(true)
         }
-        if (matchIntroPage && windowWidth<700){
+        if (matchIntroPage && windowWidth < 700) {
             setTitleVisible(false)
         }
-    },[windowWidth])
+    }, [windowWidth])
     return <>
-    <Header className={s.layoutHeader}>
-        {matchHomepage ? null : <Icon name="more" className={s.menuIcon} onClick={onClose}/>}
-        <Link to='/'>
-        <div className={s.iconAndTitle}>
-            <img src={mainLogo} className={s.mainLogo}/>
-            {titleVisible ? <h2>XING UI</h2>:null}
-        </div>
-        </Link>
-        <div className={s.headerRight}>
-            <ul>
-                <li>Vue版</li>
-            </ul>
-            <a target="_blank" href="https://github.com/DaozhangChen/Xing-react-ui"><Icon className={s.icon} name="github" /></a>
-        </div>
-    </Header>
+        <Header className={s.layoutHeader}>
+            {matchHomepage ? null : <Icon name="more" className={s.menuIcon} onClick={onClose} />}
+            <Link to='/'>
+                <div className={s.iconAndTitle}>
+                    <img src={mainLogo} className={s.mainLogo} />
+                    {titleVisible ? <h2>XING UI</h2> : null}
+                </div>
+            </Link>
+            <div className={s.headerRight}>
+                <ul>
+                    <li><a target="_blank" href="https://xing-vue-ui-1314560368.cos-website.ap-guangzhou.myqcloud.com">Vue版</a></li>
+                </ul>
+                <a target="_blank" href="https://github.com/DaozhangChen/Xing-react-ui"><Icon className={s.icon} name="github" /></a>
+            </div>
+        </Header>
     </>
 }
 
